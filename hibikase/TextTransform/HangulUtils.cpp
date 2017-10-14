ee9249488f177220145569019842e80dd1e8b98a
JosJuice <josjuice@gmail.com>
2017-10-14 18:36:39 +0200

    Reword the CC0 header
    This is meant to clarify the intended terms.

--- hibikase/TextTransform/HangulUtils.cpp
+++ hibikase/TextTransform/HangulUtils.cpp
@@ -1,7 +1,19 @@
-// You may choose to use this file under the terms of the CC0 license
-// instead of or in addition to the license of Hibikase.
-// This additional permission does not apply to Hibikase as a whole.
-// http://creativecommons.org/publicdomain/zero/1.0/
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+
+// As an additional permission for this file only, you can (at your
+// option) instead use this file under the terms of CC0.
+// <http://creativecommons.org/publicdomain/zero/1.0/>
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #include <QChar>
 #include <QString>
