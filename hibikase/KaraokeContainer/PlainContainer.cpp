3f4fbcfff79383858ca1328487c52388c4fe4214
JosJuice <josjuice@gmail.com>
2022-04-30 17:11:39 +0200

    Use SPDX license identifiers
    It's probably best to use a standard format than relying on hoping
    that the CC0 notice I wrote on my own will be interpreted as intended.

--- hibikase/KaraokeContainer/PlainContainer.cpp
+++ hibikase/KaraokeContainer/PlainContainer.cpp
@@ -1,19 +1,4 @@
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 2 of the License, or
-// (at your option) any later version.
-
-// As an additional permission for this file only, you can (at your
-// option) instead use this file under the terms of CC0.
-// <http://creativecommons.org/publicdomain/zero/1.0/>
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with this program. If not, see <http://www.gnu.org/licenses/>.
+// SPDX-License-Identifier: GPL-2.0-or-later OR CC0-1.0
 
 #include <QByteArray>
 #include <QDir>
